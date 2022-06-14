1e0de9ce59a8b4702858be30de525238c1a25161
camilaleniss <mlmariacami2@gmail.com>
2022-06-14 11:26:57 -0500

    Add tests using require for parser

---
+++ util/parser_test.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"catching-pokemons/models"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParserPokemonSuccess(t *testing.T) {
+	c := require.New(t)
+
+	body, err := ioutil.ReadFile("samples/pokeapi_response.json")
+	c.NoError(err)
+
+	var response models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &response)
+	c.NoError(err)
+
+	parsedPokemon, err := ParsePokemon(response)
+	c.NoError(err)
+
+	body, err = ioutil.ReadFile("samples/api_response.json")
+	c.NoError(err)
+
+	var expectedPokemon models.Pokemon
+
+	err = json.Unmarshal([]byte(body), &expectedPokemon)
+	c.NoError(err)
+
+	c.Equal(expectedPokemon, parsedPokemon)
+}
