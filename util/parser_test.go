95484674cc1aada5014825ae2aa71fe497a02dee
camilaleniss <mlmariacami2@gmail.com>
2022-06-14 11:27:55 -0500

    dd test for pokemon type not found

--- util/parser_test.go
+++ util/parser_test.go
@@ -33,3 +33,21 @@ func TestParserPokemonSuccess(t *testing.T) {
 
 	c.Equal(expectedPokemon, parsedPokemon)
 }
+
+func TestParserPokemonTypeNotFound(t *testing.T) {
+	c := require.New(t)
+
+	body, err := ioutil.ReadFile("samples/pokeapi_response.json")
+	c.NoError(err)
+
+	var response models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &response)
+	c.NoError(err)
+
+	response.PokemonType = []models.PokemonType{}
+
+	_, err = ParsePokemon(response)
+	c.NotNil(err)
+	c.EqualError(ErrNotFoundPokemonType, err.Error())
+}
