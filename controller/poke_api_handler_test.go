02e4afd8d5de1ed72b3f4e0f07a50affc6c8fc8d
camilaleniss <mlmariacami2@gmail.com>
2022-06-14 12:24:26 -0500

    Add tests for http calling using httpmock

--- controller/poke_api_handler_test.go
+++ controller/poke_api_handler_test.go
@@ -3,9 +3,11 @@ package controller
 import (
 	"catching-pokemons/models"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"testing"
 
+	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -24,4 +26,50 @@ func TestGetPokemonFromPokeApiSuccess(t *testing.T) {
 	c.NoError(err)
 
 	c.Equal(expected, pokemon)
+}
+
+func TestGetPokemonFromPokeApiSuccessWithMocks(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	id := "balbasaur"
+
+	body, err := ioutil.ReadFile("samples/poke_api_response.json")
+	c.NoError(err)
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	httpmock.RegisterResponder("GET", request, httpmock.NewStringResponder(200, string(body)))
+
+	pokemon, err := GetPokemonFromPokeApi(id)
+	c.NoError(err)
+
+	var expected models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &expected)
+	c.NoError(err)
+
+	c.Equal(expected, pokemon)
+}
+
+func TestGetPokemonFromPokeApiInternalServerError(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	id := "balbasaur"
+
+	body, err := ioutil.ReadFile("samples/poke_api_response.json")
+	c.NoError(err)
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	httpmock.RegisterResponder("GET", request, httpmock.NewStringResponder(500, string(body)))
+
+	_, err = GetPokemonFromPokeApi(id)
+	c.NotNil(err)
+	c.EqualError(ErrPokeApiFailure, err.Error())
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,14 @@ module catching-pokemons
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jarcoal/httpmock v1.2.0
+	github.com/stretchr/testify v1.7.2
+)
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/stretchr/testify v1.7.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
